00e731cfa356f55746780e44a7c942f1dc662d3c
MTarshi <106284081+MTarshi@users.noreply.github.com>
2022-08-24 20:03:51 -0400

    temperature check basic

---
+++ src/test/java/classone/gitBasics.java
@@ -0,0 +1,13 @@
+package classone;
+
+
+
+public class gitBasics {
+
+    public static void main(String[] args) {
+        System.out.println("today is a beuatiful day");
+    }
+    //write down a code that checks the temprature from website and then
+    //print out that is in a beuatiful day or not.
+
+}
