312b531684616af083d78502385cc09a261cf2d7
MTarshi <106284081+MTarshi@users.noreply.github.com>
2022-08-27 10:38:59 -0400

    Three lines of additional codes

--- src/test/java/classone/gitBasics.java
+++ src/test/java/classone/gitBasics.java
@@ -6,6 +6,9 @@ public class gitBasics {
 
     public static void main(String[] args) {
         System.out.println("today is not a beuatiful day");
+        System.out.println("today is not a beuatiful day");
+        System.out.println("today is not a beuatiful day");
+        System.out.println("today is not a beuatiful day");
     }
     //write down a code that checks the temprature from website and then
     //print out that is in a beuatiful day or not.
